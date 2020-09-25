c1fa3d09be63552f053d35f4cd8a0bd8223f060c
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-09-25 22:56:28 +0900

    create: useLocalStorage custom hook

--- src/hook/useLocalStorage.ts
+++ src/hook/useLocalStorage.ts
@@ -1,5 +1,26 @@
 import { useState, useEffect } from 'react';
 
-export const useLocalStorage = () => {
+const PREFIX: string = 'codepen-clone-';
+
+
+export const useLocalStorage = (key: string, initialValue: string) => {
+  const prefixKey: string = PREFIX + key;
+  const [value, setValue] = useState(() => {
+    const jsonValue = localStorage.getItem(prefixKey);
+    if (jsonValue != null) return JSON.parse(jsonValue);
+
+    try {
+      if(typeof initialValue !== 'function') {
+        return initialValue;
+      }
+    } catch (err) {
+      console.log(`${err}`);
+    }
+  })
+
+  useEffect(() => {
+    localStorage.setItem(prefixKey, JSON.stringify(value))
+  }, [prefixKey, value])
   
+  return [value, setValue];
 }
