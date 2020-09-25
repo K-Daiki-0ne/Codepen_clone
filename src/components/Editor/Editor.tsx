123d281cfe9d0412014093bac5c3df5db53c4809
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-09-25 22:30:00 +0900

    create: base fiel

---
+++ src/components/Editor/Editor.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Editor: React.FC = ({ 
+
+}): JSX.Element => {
+  return (
+    <div>
+
+    </div>
+  )
+}
---
+++ src/page/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Home: React.FC = (): JSX.Element => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
