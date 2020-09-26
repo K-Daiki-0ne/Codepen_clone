e01658479bc13879220a36baac099fe1fce43d63
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-09-26 22:05:47 +0900

    add: basic Home component function

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocalStorage } from '../hook/useLocalStorage';
 
 export const Home: React.FC = (): JSX.Element => {
+  const [html, setHtml] = useLocalStorage('html', '');
+  const [css, setCss] = useLocalStorage('css', '');
+  const [js, setJs] = useLocalStorage('js', '');
+  const [srcDoc, setSrcDoc] = useState<string>('');
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>
+      `)
+    }, 250)
+
+    return () => clearTimeout(timeout)
+  }, [html, css, js])
+  
   return (
     <div>
-      
+
     </div>
   )
 }
