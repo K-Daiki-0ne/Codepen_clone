e107ec132c062d421d2791cb030c870a4584cf81
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-09-27 21:42:07 +0900

    add: React Fragment

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -24,25 +24,37 @@ export const Home: React.FC = (): JSX.Element => {
   }, [html, css, js])
 
   return (
-    <div className='pane top-pane'>
-      <Editor 
-        language="xml"
-        displayName="HTML"
-        value={html}
-        onChange={setHtml}
-      />
-      <Editor 
-        language="css"
-        displayName="Css"
-        value={css}
-        onChange={setCss}
-      />
-      <Editor 
-        language="javascript"
-        displayName="JS"
-        value={js}
-        onChange={setJs}
-      />
-    </div>
+    <>
+      <div className='pane top-pane'>
+        <Editor 
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor 
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor 
+          language="javascript"
+          displayName="JS"
+          value={js}
+          onChange={setJs}
+        />
+      </div>
+      <div className="pane">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
   )
 }
