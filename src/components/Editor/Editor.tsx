ffacaf28f9d665ee3ff68e4e68fc2bda09d72e39
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-09-27 18:22:57 +0900

    create: Editor component

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,11 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+import 'codemirror/mode/xml/xml';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/css/css';
+import { Controlled as ControlledEditor } from 'react-codemirror2';
 
-export const Editor: React.FC = ({ 
+type Props = {
+  props: EditorProps
+}
 
+type EditorProps = {
+  language: string;
+  displayName: string;
+  value: string;
+  onChange: () => void;
+}
+
+export const Editor: React.FC<Props> = ({ 
+  props
 }): JSX.Element => {
-  return (
-    <div>
+  const {
+    language,
+    displayName,
+    value,
+    onChange
+  } = props
+  const [open, setOpen] = useState<boolean>(true);
 
+  const handleChange = (editor: any, data: any, value: string): void => {
+    onChange(value)
+  }
+
+  return (
+    <div className='editor-container'>
+      <div className='editor-title'>
+        { displayName }
+        <button
+          type='button'
+          className='expand-collapse-btn'
+          onClick={() => setOpen(prevOpen => !prevOpen)}
+        >
+          1/2
+        </button>
+      </div>
+      <ControlledEditor
+        onBeforeChange={handleChange}
+        value={value}
+        className="code-mirror-wrapper"
+        options={{
+          lineWrapping: true,
+          lint: true,
+          mode: language,
+          theme: 'material',
+          lineNumbers: true
+        }}
+      />
     </div>
   )
 }
