e9bb8171363f279a2276b0502c26e46ace4e5649
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-09-27 17:53:18 +0900

    add: connect Home component to Root component

--- src/root/App.tsx
+++ src/root/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Home } from '../page/Home';
 import './App.scss';
 
 const App: React.FC = (): JSX.Element => {
   return (
     <div className="App">
-      
+      <Home />
     </div>
   );
 }
