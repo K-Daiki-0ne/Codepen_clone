3ac1b355a57251bb219577f58594d5fc1f85148b
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-10-03 14:47:20 +0900

    create: Home component test code

---
+++ src/__tests__/Home.spec.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Home } from '../page/Home';
+import { Editor } from '../components/index'
+import { unmountComponentAtNode } from "react-dom";
+
+let container: any = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+describe('Home Component Test', () => {
+  const wrapper = shallow(<Home />);
+
+  describe('rendering test', () => {
+    const wrap = wrapper.find('div');
+    
+    test('Home Component', () => expect(wrap).toBeTruthy());
+
+    describe('children component test', () => {
+      const editor = wrap.find(Editor);
+      expect(editor).toBeTruthy();
+    });
+
+    describe('iframe test', () => {
+      const frame = wrap.find('iframe');
+      expect(frame).toBeTruthy();
+    });
+
+  })
+})
