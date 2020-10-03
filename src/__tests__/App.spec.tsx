d50c7db302cb55c5d8bc0c65d009827906f5c5ca
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-10-03 14:14:11 +0900

    create: root component test code

---
+++ src/__tests__/App.spec.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../root/App';
+import { Home } from '../page/Home';
+
+describe('App.tsx ', () => {
+  const wrapper = shallow(<App />);
+
+  describe('rendering test', () => {
+    const wrap = wrapper.find('div');
+    test('root', () => expect(wrap).toBeTruthy());
+    test('children component', () => expect(wrap.find(Home)).toBeTruthy());
+  })
+})
